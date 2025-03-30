87ed30daad4ee34ec792cdf2c68162b08fc74e95
Aaryan Praveen <55227143+boioboi16@users.noreply.github.com>
2025-03-30 17:33:22 -0400

    bug fixes

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -6,11 +6,12 @@ const server = new WebSocketServer({
 });
 
 server.on("connection", async (socket) => {
-    client.user.create({
+    const res = await client.user.create({
         data: {
             username: Math.random().toString(),
             password: Math.random().toString()
         }
     })
+    console.log(res);
     socket.send("Hi there you are connected to the server");
 })
